a9a733689efcba759c448910ea4236f4763cb3e3
Indra <indra.roadota@gmail.com>
2021-12-03 21:03:21 +0700

    bit

--- Logic.php
+++ Logic.php
@@ -1,3 +1,4 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <?php 
 include "navbar.php";
 
@@ -10,8 +11,8 @@ include "navbar.php";
     
     <title>The DAWs Article</title>
     <link rel="icon" href="daw.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    >
+
+    
 
 
 </head>
